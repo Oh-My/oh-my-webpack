ef560ca6c4f49844b99779851aa9f9c46ec4aeae
Johan Eriksson <johan.eriksson@ohmy.nu>
2019-10-01 11:33:54 +0200

    Update postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,26 +5,32 @@ const type = guess_project_type();
 
 let file = fs.readFileSync(path.resolve(__dirname, '../src/webpack.config.js'), 'utf8');
 
-if (type === 'wordpress') {
-    file = file.replace(/\$PUBLIC_PATH\$/g, "'web'")
-        .replace(/\$OUT\$/g, "'web/wp-content/themes/{{cookiecutter.project_slug}}/build'")
-        .replace(/\$PURGECSS\$/g, "'./web/wp-content/**/*.php'")
-        .replace(/\$WATCH\$/g, "'web/wp-content/**/*.php'");
+const defaults = {
+    wordpress: {
+        PUBLIC_PATH: 'web',
+        OUT: 'web/wp-content/themes/{{cookiecutter.project_slug}}/build',
+        PURGECSS: './web/wp-content/**/*.php',
+        WATCH: 'web/wp-content/**/*.php'
+    },
+    laravel: {
+        PUBLIC_PATH: 'public',
+        OUT: 'public/build',
+        PURGECSS: './resources/views/**/**.blade.php',
+        WATCH: 'resources/views/**/**.blade.php'
+    },
+    other: {
+        PUBLIC_PATH: 'web',
+        OUT: 'web/build',
+        PURGECSS: null,
+        WATCH: null
+    }
 }
 
-if (type === 'laravel') {
-    file = file.replace(/\$PUBLIC_PATH\$/g, "'public'")
-        .replace(/\$OUT\$/g, "'public/build'")
-        .replace(/\$PURGECSS\$/g, "'./resources/views/**/**.blade.php'")
-        .replace(/\$WATCH\$/g, "'resources/views/**/**.blade.php'");
-}
-
-if (type === 'other') {
-    file = file.replace(/\$PUBLIC_PATH\$/g, "'web'")
-        .replace(/\$OUT\$/g, "'web/build'")
-        .replace(/\$PURGECSS\$/g, '// ..')
-        .replace(/\$WATCH\$/g, '// ..');
-}
+Object.entries(defaults[type]).forEach(([key, value]) => {
+    file = value === null
+        ? file.replace(new RegExp('\\$'+key+'\\$', 'g'), "// ..")
+        : file.replace(new RegExp('\\$'+key+'\\$'), "'"+value+"'");
+});
 
 fs.writeFileSync(path.join(process.cwd(), 'webpack.config.js'), file, 'utf8');
 
